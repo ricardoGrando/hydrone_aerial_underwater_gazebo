7062bf64523b431406260f301e120ff0ff0ab25e
ricardogrando <ricardogrando13@gmail.com>
2020-09-04 15:12:26 -0300

    whatever

--- hydrone_aerial_underwater_ddpg/scripts/store_disk.py
+++ hydrone_aerial_underwater_ddpg/scripts/store_disk.py
@@ -8,7 +8,7 @@ NAME = 'ddpg_env2_2d'
 
 def store_disk(data):
     global NAME
-    file_object = open('~/catkin_ws/src/hydrone_aerial_underwater_gazebo/hydrone_aerial_underwater_ddpg/scripts/'+NAME+'.csv', 'a')
+    file_object = open('~/catkin_ws/src/hydrone_aerial_underwater_gazebo/hydrone_aerial_underwater_ddpg/scripts/Models/ddpg_stage_1/'+NAME+'.csv', 'a')
     file_object.write(data.data+'\n')
 
 def pose_callback(data):
