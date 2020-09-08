eef06652e39e945f4618e1a9cbaead242f32a964
ricardogrando <ricardogrando13@gmail.com>
2020-09-08 19:31:48 -0300

    d21

--- hydrone_aerial_underwater_ddpg/scripts/ddpg_stage_1.py
+++ hydrone_aerial_underwater_ddpg/scripts/ddpg_stage_1.py
@@ -342,7 +342,7 @@ print('Action Max: ' + str(ACTION_V_MAX) + ' m/s and ' + str(ACTION_W_MAX) + ' r
 ram = MemoryBuffer(MAX_BUFFER)
 trainer = Trainer(STATE_DIMENSION, ACTION_DIMENSION, ACTION_V_MAX, ACTION_W_MAX, ram)
 noise = OUNoise(ACTION_DIMENSION, max_sigma=.71, min_sigma=0.2, decay_period=8000000)
-ep_start = 260
+ep_start = 740
 trainer.load_models(ep_start)
 
 if __name__ == '__main__':
