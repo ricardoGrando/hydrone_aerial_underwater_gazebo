7ef830f0c495932cc91a3ef36e950fddcf8ee976
ricardoGrando <ricardogrando13@gmail.com>
2021-01-07 13:25:33 -0300

    sdgfasdg

--- hydrone_aerial_underwater_ddpg/scripts/ddpg_air3D_tanh_lstm.py
+++ hydrone_aerial_underwater_ddpg/scripts/ddpg_air3D_tanh_lstm.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from collections import deque
 from std_msgs.msg import *
-from environment_2D import Env
+from environment_3D import Env
 import torch
 import torch.nn.functional as F
 import gc
@@ -107,8 +107,6 @@ class Critic(nn.Module):
         # xs = torch.relu(self.fc1(state))
         # xa = torch.relu(self.fa1(action))
         # x = torch.cat((xs,xa), dim=1)
-        # print(state.is_cuda)
-        # print(action.is_cuda)
         x_state_action = torch.cat([state, action], 1)
         x = torch.relu(self.fca1(x_state_action))
         x = torch.relu(self.fca2(x))
@@ -146,13 +144,16 @@ class Actor(nn.Module):
         # print(action.is_cuda)
         action = self.fa1(x.reshape(self.layer_size)).squeeze(0)#.to(device=self.device)
         # print(action.is_cuda)
-        
+
         if state.shape <= torch.Size([self.state_dim]):
             action[0] = ((torch.tanh(action[0]) + 1.0)/2.0)*self.action_limit_v
             action[1] = torch.tanh(action[1])*self.action_limit_w
+            action[2] = torch.tanh(action[2])*self.action_limit_w
         else:
             action[:,0] = ((torch.tanh(action[:,0]) + 1.0)/2.0)*self.action_limit_v
             action[:,1] = torch.tanh(action[:,1])*self.action_limit_w
+            action[:,2] = torch.tanh(action[:,2])*self.action_limit_w
+
         return action
 
     def forward(self, state):
@@ -311,9 +312,9 @@ MAX_STEPS = 500
 MAX_BUFFER = 50000
 rewards_all_episodes = []
 
-STATE_DIMENSION = 24
+STATE_DIMENSION = 26
 LASER_SAMPLES = 20
-ACTION_DIMENSION = 2
+ACTION_DIMENSION = 3
 ACTION_V_MAX = 0.25 # m/s
 ACTION_V_MIN = 0.0
 ACTION_W_MAX = 0.25 # rad
@@ -325,7 +326,8 @@ print('Action Dimensions: ' + str(ACTION_DIMENSION))
 print('Action Max: ' + str(ACTION_V_MAX) + ' m/s and ' + str(ACTION_W_MAX) + ' rad')
 replay_buffer = ReplayBuffer(MAX_BUFFER)
 trainer = Trainer(STATE_DIMENSION, ACTION_DIMENSION, ACTION_V_MAX, ACTION_W_MAX, replay_buffer)
-noise = OUNoise(ACTION_DIMENSION, max_sigma=.71, min_sigma=0.2, decay_period=8000000)
+# noise = OUNoise(ACTION_DIMENSION, max_sigma=.71, min_sigma=0.2, decay_period=8000000)
+noise = OUNoise(ACTION_DIMENSION, max_sigma=.075, min_sigma=0.03, decay_period=8000000)
 
 if __name__ == '__main__':
     global world
@@ -373,12 +375,14 @@ if __name__ == '__main__':
                 N = copy.deepcopy(noise.get_noise(t=step))                
                 N[0] = N[0]*ACTION_V_MAX/2
                 N[1] = N[1]*ACTION_W_MAX
+                N[2] = N[2]*ACTION_W_MAX
                 # action[0] = action[0] + N[0]
                 # action[1] = action[1] + N[1]
                 # rospy.loginfo("Noise: %s, %s", str(N[0]), str(N[1]))
                 # rospy.loginfo("Action before: %s, %s", str(action[0]), str(action[1]))
                 action[0] = np.clip(action[0] + N[0], ACTION_V_MIN, ACTION_V_MAX)
                 action[1] = np.clip(action[1] + N[1], ACTION_W_MIN, ACTION_W_MAX)
+                action[2] = np.clip(action[2] + N[2], ACTION_W_MIN, ACTION_W_MAX)
             else:
                 action = trainer.get_exploration_action(state)
 
@@ -414,9 +418,18 @@ if __name__ == '__main__':
                     result = (str(ep)+','+str(rewards_current_episode))
                     pub_result.publish(result)
                 break
+
+            # if (reward == 100):
+            #     is_training = False
+            #     break
+
         if ep%20 == 0:
             trainer.save_models(ep)
 
 print('Completed Training')
 
-# roslaunch hydrone_aerial_underwater_ddpg deep_RL_2D.launch ep:=0 file_dir:=ddpg_stage_1_air2D_tanh_lstm deep_rl:=ddpg_air2D_tanh_lstm.py world:=stage_1_aerial root_dir:=/home/ricardo/ graphic_int:=false
+# roslaunch hydrone_aerial_underwater_ddpg deep_RL_2D.launch ep:=0 file_dir:=ddpg_stage_1_air2D_tanh_3layers deep_rl:=ddpg_air2D_tanh_3layers.py world:=stage_1_aerial root_dir:=/home/ricardo/
+
+# roslaunch hydrone_aerial_underwater_ddpg deep_RL_2D.launch ep:=380 file_dir:=ddpg_stage_1_air2D_tanh_3layers deep_rl:=ddpg_air2D_tanh_3layers.py world:=stage_1_aerial root_dir:=/home/ricardo/ graphic_int:=false
+
+# roslaunch hydrone_aerial_underwater_ddpg deep_RL_2D.launch ep:=1000 file_dir:=ddpg_stage_1_air3D_tanh_3layers deep_rl:=ddpg_air3D_tanh_3layers.py world:=stage_1 root_dir:=/home/ricardo/ graphic_int:=true testing:=true x:=2.0 y:=3.0 z:=-1.0 arr_distance:=0.25 testing_eps:=200
